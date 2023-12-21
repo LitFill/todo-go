6f93695c02b4312c504f55b0d34c2821a57aa2a0
LitFill <98679748+LitFill@users.noreply.github.com>
2023-12-22 05:18:19 +0700

    Menambahkan fungsi todo list ke aplikasi
    - Mengubah file main.go untuk menambahkan fungsi todo list.
    - Menambahkan endpoint /todos yang mendukung metode GET dan POST.
    - Metode GET akan menampilkan semua todo.
    - Metode POST akan menambahkan todo baru ke daftar.
    - Menambahkan penanganan error untuk metode request yang tidak diizinkan.

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+var todos = []string{}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/todos", func(writer http.ResponseWriter, request *http.Request) {
+		switch request.Method {
+		case "POST":
+			// tambahkan todo baru
+			todoBaru := request.URL.Query().Get("todo")
+			todos = append(todos, todoBaru)
+			fmt.Fprintf(writer, "Todo baru telah ditambahkan, %s\n", todoBaru)
+		case "GET":
+			// tampilkan semua todo
+			for _, todo := range todos {
+				fmt.Fprintln(writer, todo)
+			}
+		default:
+			http.Error(writer, "Metode Request tidak diizinkan", http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.ListenAndServe(":1842", nil)
 }
